26bf56584037c501b479cdd94113d76a59816b1d
zy <1661996964@qq.com>
2022-10-22 15:38:36 +0800

    将请求响应向后传递，被后续的Filter或Servlet处理

--- src/main/java/com/zy/filter/FirstFilter.java
+++ src/main/java/com/zy/filter/FirstFilter.java
@@ -12,7 +12,8 @@ public class FirstFilter implements Filter {
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
         System.out.println("过滤器已生效");
-
+        //将请求响应向后传递，被后续的Filter或Servlet处理
+        filterChain.doFilter(servletRequest,servletResponse);
     }
 
     @Override
