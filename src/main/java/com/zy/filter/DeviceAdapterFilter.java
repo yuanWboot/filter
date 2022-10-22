775e699ded932894832c6a09e4f69a25d917b328
zy <1661996964@qq.com>
2022-10-23 00:36:13 +0800

    uri中有了头地址则向请求向后传递

--- src/main/java/com/zy/filter/DeviceAdapterFilter.java
+++ src/main/java/com/zy/filter/DeviceAdapterFilter.java
@@ -15,8 +15,8 @@ public class DeviceAdapterFilter implements Filter {
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
         //将Servlet强转为http接口
-        HttpServletRequest req = (HttpServletRequest)servletRequest;
-        HttpServletResponse resp = (HttpServletResponse)servletResponse;
+        HttpServletRequest req = (HttpServletRequest) servletRequest;
+        HttpServletResponse resp = (HttpServletResponse) servletResponse;
         String uri = req.getRequestURI();//index.html >>/desktop/index.html  >>/mobile/index.html
         String userAgent = req.getHeader("user-agent");
         String targetUri = null;
