3e21f4b191e4ce14d7aad640dab95599e7996a2c
zy <1661996964@qq.com>
2022-10-22 22:35:45 +0800

    创建filterA  filterB filterC

---
+++ src/main/java/com/zy/filter/chain/filterC.java
@@ -0,0 +1,23 @@
+package com.zy.filter.chain;
+
+import javax.servlet.*;
+import java.io.IOException;
+
+public class filterC implements Filter {
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+
+        System.out.println("I'm filter C");
+        filterChain.doFilter(servletRequest,servletResponse);
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
