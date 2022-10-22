4328300332248324cafd8ec8869888af5194027e
zy <1661996964@qq.com>
2022-10-22 17:18:23 +0800

    为请求和响应设置过滤字符集为UTF-8

--- src/main/java/com/zy/filter/CharacterEncodingFilter.java
+++ src/main/java/com/zy/filter/CharacterEncodingFilter.java
@@ -2,6 +2,7 @@ package com.zy.filter;
 
 import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 public class CharacterEncodingFilter implements Filter {
@@ -14,7 +15,10 @@ public class CharacterEncodingFilter implements Filter {
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
        //将Servlet强转为http接口
         HttpServletRequest req = (HttpServletRequest)servletRequest;
-        HttpServletRequest reqs =(HttpServletRequest) servletResponse;
+        HttpServletResponse resp =(HttpServletResponse) servletResponse;
+        //为请求和响应设置过滤字符集为UTF-8
+        req.setCharacterEncoding("UTF-8");
+        resp.setContentType("text/html;charset=utf-8");
     }
 
     @Override
