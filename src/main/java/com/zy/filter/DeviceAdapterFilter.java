4ab8ac908637ce18703b49623dd20345449b2102
zy <1661996964@qq.com>
2022-10-22 23:43:58 +0800

    //将Servlet强转为http接口

--- src/main/java/com/zy/filter/DeviceAdapterFilter.java
+++ src/main/java/com/zy/filter/DeviceAdapterFilter.java
@@ -1,6 +1,8 @@
 package com.zy.filter;
 
 import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 public class DeviceAdapterFilter implements Filter {
@@ -12,6 +14,11 @@ public class DeviceAdapterFilter implements Filter {
 
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        //将Servlet强转为http接口
+        HttpServletRequest req = (HttpServletRequest)servletRequest;
+        HttpServletResponse resp = (HttpServletResponse)servletResponse;
+        String uri = req.getRequestURI();
+        String userAgent = req.getHeader("user-agent");
 
     }
 
