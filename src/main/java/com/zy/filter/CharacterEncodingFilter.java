b64757cb70a439d7df8a15626334d9213b8207c6
zy <1661996964@qq.com>
2022-10-22 17:13:08 +0800

    将Servlet强转为http接口

--- src/main/java/com/zy/filter/CharacterEncodingFilter.java
+++ src/main/java/com/zy/filter/CharacterEncodingFilter.java
@@ -1,6 +1,7 @@
 package com.zy.filter;
 
 import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
 
 public class CharacterEncodingFilter implements Filter {
@@ -11,7 +12,9 @@ public class CharacterEncodingFilter implements Filter {
 
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
-
+       //将Servlet强转为http接口
+        HttpServletRequest req = (HttpServletRequest)servletRequest;
+        HttpServletRequest reqs =(HttpServletRequest) servletResponse;
     }
 
     @Override
